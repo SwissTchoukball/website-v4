46770d10b348eb01d8be4ae6387dd80d4375b67d
David Sandoz <david.sandoz@epfl.ch>
2015-09-11 18:53:47 +0200

    Cleaning

--- ds.php
+++ ds.php
@@ -1,17 +1,12 @@
-<? // À vérifier, mais ce fichier n'a plus l'air d'être utilisé (supprimer.auto.connexion.php s'occupe de tout)
+<?php
+
+// À vérifier, mais ce fichier n'a plus l'air d'être utilisé (supprimer.auto.connexion.php s'occupe de tout)
 
 session_start();
 include "includes/var.href.inc.php";
-
-
-
-
-
-
 
 session_destroy();
 
 $host = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-header("Location: http://$host$uri/".VAR_HREF_PAGE_PRINCIPALE,true);
-?>
+header("Location: http://$host$uri/".VAR_HREF_PAGE_PRINCIPALE, true);
