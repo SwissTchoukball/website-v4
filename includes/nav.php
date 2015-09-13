139141db825c84e72fd06adfafc4790b0cea46e5
David Sandoz <david.sandoz@epfl.ch>
2015-09-13 16:29:57 +0200

    Added a const PATH_TO_ROOT to make the development possible with any URL

--- includes/nav.php
+++ includes/nav.php
@@ -1,57 +1,57 @@
 <nav id="main-nav">
 <?php
-	$navQuery = "SELECT sm.id, m.nom" . $_SESSION['__langue__'] . " AS menu, sm.nomFr AS sousmenu, m.id AS sousMenuDeId, sm.ordre, m.userLevel AS parentUserLevel, sm.userLevel, sm.urlRewriting AS link, m.urlRewriting AS parentLink
-				 FROM " . $typemenu . " m
-				 LEFT OUTER JOIN " . $typemenu . " sm ON sm.sousMenuDeId = m.id
-				 WHERE m.sousMenuDeId = -1
-				 ORDER BY m.ordre, sm.ordre";
-	if (!$navResult = mysql_query($navQuery)) {
-		printErrorMessage('Erreur lors du chargement du menu');
-	} else {
-		$previousNavParentItemID = 0;
-		while ($nav = mysql_fetch_assoc($navResult)) {
-			$navParentItemID = $nav['sousMenuDeId'];
-			$navItemOrder = $nav['ordre'];
-			$navParentItemName = $nav['menu'];
-			$navItemName = $nav['sousmenu'];
-			$navParentUserLevel = $nav['parentUserLevel'];
-			$navUserLevel = $nav['userLevel'];
-			if ($nav['link'] != '') {
-				$navLink = $nav['link'];
-			} else {
-				$srcFile = $admin ? '/admin.php' : '/index.php';
-				$navLink = $srcFile . '?menuselection=' . $navParentItemID . '&smenuselection=' . $navItemOrder;
-			}
-			$navParentLink = $nav['parentLink'];
+$navQuery = "SELECT sm.id, m.nom" . $_SESSION['__langue__'] . " AS menu, sm.nomFr AS sousmenu, m.id AS sousMenuDeId, sm.ordre, m.userLevel AS parentUserLevel, sm.userLevel, sm.urlRewriting AS link, m.urlRewriting AS parentLink
+             FROM " . $typemenu . " m
+             LEFT OUTER JOIN " . $typemenu . " sm ON sm.sousMenuDeId = m.id
+             WHERE m.sousMenuDeId = -1
+             ORDER BY m.ordre, sm.ordre";
+if (!$navResult = mysql_query($navQuery)) {
+    printErrorMessage('Erreur lors du chargement du menu');
+} else {
+    $previousNavParentItemID = 0;
+    while ($nav = mysql_fetch_assoc($navResult)) {
+        $navParentItemID = $nav['sousMenuDeId'];
+        $navItemOrder = $nav['ordre'];
+        $navParentItemName = $nav['menu'];
+        $navItemName = $nav['sousmenu'];
+        $navParentUserLevel = $nav['parentUserLevel'];
+        $navUserLevel = $nav['userLevel'];
+        if ($nav['link'] != '') {
+            $navLink = $nav['link'];
+        } else {
+            $srcFile = $admin ? 'admin.php' : 'index.php';
+            $navLink = $srcFile . '?menuselection=' . $navParentItemID . '&smenuselection=' . $navItemOrder;
+        }
+        $navParentLink = $nav['parentLink'];
 
 
-			if ($navParentUserLevel >= $_SESSION['__userLevel__']) {
-				if ($navParentItemID != $previousNavParentItemID) {
-					if ($previousNavParentItemID != 0) {
-						echo '</ul>';
-					}
+        if ($navParentUserLevel >= $_SESSION['__userLevel__']) {
+            if ($navParentItemID != $previousNavParentItemID) {
+                if ($previousNavParentItemID != 0) {
+                    echo '</ul>';
+                }
 
-					echo $menuselection == $navParentItemID ? '<h1 class="open">' : '<h1>';
+                echo $menuselection == $navParentItemID ? '<h1 class="open">' : '<h1>';
 
-					if (is_null($navItemOrder) && $navParentLink != '') {
-						echo '<a href="' . $navParentLink . '">' . $navParentItemName . '</a>';
-					} else {
-						echo $navParentItemName;
-					}
+                if (is_null($navItemOrder) && $navParentLink != '') {
+                    echo '<a href="'. PATH_TO_ROOT . $navParentLink . '">' . $navParentItemName . '</a>';
+                } else {
+                    echo $navParentItemName;
+                }
 
-					echo '</h1>';
-					echo $menuselection == $navParentItemID ? '<ul style="display: block;">' : '<ul>';
+                echo '</h1>';
+                echo $menuselection == $navParentItemID ? '<ul style="display: block;">' : '<ul>';
 
-				}
+            }
 
-				if ($navUserLevel >= $_SESSION['__userLevel__']) {
-					$navItemClass = ($menuselection == $navParentItemID && $smenuselection == $navItemOrder) ? 'open' : '';
-					echo '<li><a href="' . $navLink . '" class="' . $navItemClass . '">' . $navItemName . '</a></li>';
-				}
-			}
+            if ($navUserLevel >= $_SESSION['__userLevel__']) {
+                $navItemClass = ($menuselection == $navParentItemID && $smenuselection == $navItemOrder) ? 'open' : '';
+                echo '<li><a href="' . PATH_TO_ROOT . $navLink . '" class="' . $navItemClass . '">' . $navItemName . '</a></li>';
+            }
+        }
 
-			$previousNavParentItemID = $navParentItemID;
-		}
-	}
+        $previousNavParentItemID = $navParentItemID;
+    }
+}
 ?>
 </nav>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,16 +1,18 @@
 <div id="sidebar">
-	<?php include('includes/nav.php'); ?>
-	<div id="partners">
-		<?php
-		echo "<a href='/partenaires'>Nos partenaires</a>";
+    <?php include('includes/nav.php'); ?>
+    <div id="partners">
+        <?php
+        echo "<a href='/partenaires'>Nos partenaires</a>";
         $requeteSQL = "SELECT * FROM Sponsors WHERE afficherFooter>='1' ORDER BY afficherFooter";
         $recordset= mysql_query($requeteSQL);
-        while($record = mysql_fetch_array($recordset)){
+        while ($record = mysql_fetch_array($recordset)) {
             echo "<a href='".$record["lienWeb"]."' target='_blank'>";
-            echo "<img class='logoSponsorsMenu' src='/".$record["lienLogoSidebar"]."' />";
+            echo "<img class='logoSponsorsMenu' src='" . PATH_TO_ROOT . "".$record["lienLogoSidebar"]."' />";
             echo "</a>";
         }
-		?>
-	</div>
-	<div class="hashtag">#tchouksuisse</div>
+        ?>
+    </div>
+    <div class="hashtag">#tchouksuisse</div>
+    <?php /* Do not add any space, new line or even any character after the following closing
+             div tag. This will alter seriously the layout of the website */ ?>
 </div>
