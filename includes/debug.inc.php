584bdce4205060c92d241fdae4342a2f80cf0aa6
David Sandoz <david.sandoz@epfl.ch>
2015-09-13 15:15:30 +0200

    Started cleaning files

--- includes/debug.inc.php
+++ includes/debug.inc.php
@@ -1,6 +1,5 @@
-<?
+<?php
 // fichier permettant d'afficher des informations pour aider au développement
-$_SESSION["debug"]=false;// affiche les texts utilisés au développement
+$_SESSION["debug"] = false;// affiche les texts utilisés au développement
 
-$_SESSION["debug_tracage"]=false; // affiche tous les fichiers appelés dans une page
-?>
+$_SESSION["debug_tracage"] = false; // affiche tous les fichiers appelés dans une page
