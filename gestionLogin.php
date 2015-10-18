7ce9de323bf6345ea0c5421e696adb9129ef70fd
David Sandoz <david.sandoz@epfl.ch>
2015-10-18 17:49:20 +0200

    Adapting login for local use

--- gestionLogin.php
+++ gestionLogin.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+    require('config-dev.php');
+    $devWebsite = true;
+} else {
+    require('config.php');
+    $devWebsite = false;
+}
+
 include "includes/var.href.inc.php";
 
 
@@ -10,10 +19,6 @@ if ($_SESSION["__userLevel__">100]) {
     header("Location: http://$host$uri".VAR_HREF_PAGE_ADMIN, true);
 }
 
-@mysql_pconnect("localhost", "kuix_tchoukball", "jsdQs22YG1X92jW") or die ("<H1>Connexion impossible au serveur</H1>");
-@mysql_select_db("kuix_tchoukball1") or die ("<H1>Connexion impossible à la base de donnée</H1>");
-mysql_set_charset('latin1');
-
 // se faire passer pour la partie admin
 $PHP_SELF = "/admin.php";
 
@@ -21,7 +26,9 @@ $PHP_SELF = "/admin.php";
 if (isset($_POST["login"]) && isset($_POST["username"]) && isset($_POST["password"])) {
     $usernameLogin = strtolower(mysql_escape_string(strip_tags($_POST["username"])));
 
-    $requeteSQL = "SELECT p.id, nom, prenom, username, userLevel, password, idClub, gestionMembresClub, c.nbIdClub FROM `Personne` p, `ClubsFstb` c WHERE p.`username`='".$usernameLogin."' AND p.`idClub`=c.`id`";
+    $requeteSQL = "SELECT p.id, nom, prenom, username, userLevel, password, idClub, gestionMembresClub, c.nbIdClub
+                   FROM `Personne` p, `ClubsFstb` c
+                   WHERE p.`username`='".$usernameLogin."' AND p.`idClub`=c.`id`";
     $resultatSQL = mysql_query($requeteSQL);
     if (!$resultatSQL) {
         header("Location: http://$host$uri/login-fail-4", true);
@@ -32,8 +39,8 @@ if (isset($_POST["login"]) && isset($_POST["username"]) && isset($_POST["passwor
             exit();
         }
 
-        echo $requeteSQL."<br />";
-        echo md5($_POST["password"])."==".$record["password"];
+        //echo $requeteSQL."<br />";
+        //echo md5($_POST["password"])."==".$record["password"];
 
 
         if (md5($_POST["password"])==$record["password"]) {
@@ -57,7 +64,8 @@ if (isset($_POST["login"]) && isset($_POST["username"]) && isset($_POST["passwor
 
             // Insertion du login dans l'historique des logins
             $maintenant = getdate();
-            $requeteSQL = "INSERT INTO `HistoriqueLogin` ( `username` , `date` , `heure` ) VALUES ('".$record["username"]."', '".$maintenant["year"]."-".$maintenant["mon"]."-".$maintenant["mday"]."', '".$maintenant["hours"].":".$maintenant["minutes"].":".$maintenant["seconds"]."')";
+            $requeteSQL = "INSERT INTO `HistoriqueLogin` ( `username` , `date` , `heure` )
+                           VALUES ('".$record["username"]."', '".$maintenant["year"]."-".$maintenant["mon"]."-".$maintenant["mday"]."', '".$maintenant["hours"].":".$maintenant["minutes"].":".$maintenant["seconds"]."')";
             mysql_query($requeteSQL);
 
             header("Location: http://$host$uri".VAR_HREF_PAGE_ADMIN, true);
