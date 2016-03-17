9bfc580908d91ffe6ff2c4fd70212078cb93cf49
David Sandoz <david.sandoz@epfl.ch>
2016-03-17 18:21:14 +0100

    Updated hard-coded season years

--- pages/formule.championnat.inc.php
+++ pages/formule.championnat.inc.php
@@ -1,4 +1,4 @@
-<h2>Championnat 2013-2014</h2>
+<h2>Championnat 2015-2016</h2>
 <img src='/pictures/schema_championnat.png' alt='schéma championnat' width="809px" />
 <!--<h2>Championnat M18 2010-2011</h2>
 <img src='pictures/schema_championnatM18.png' alt='schéma championnat M18' />-->
