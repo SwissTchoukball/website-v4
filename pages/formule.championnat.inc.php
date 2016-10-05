213aae8e9cd424f6a6072fecf41123d4cfa2df1b
David Sandoz <david.sandoz@tchoukball.ch>
2016-10-05 20:12:01 +0200

    Automated the content of the championship formula page

--- pages/formule.championnat.inc.php
+++ pages/formule.championnat.inc.php
@@ -1,6 +1,18 @@
-<h2>Championnat 2015-2016</h2>
-<img src='/pictures/schema_championnat.png' alt='schéma championnat' width="809px" />
-<!--<h2>Championnat M18 2010-2011</h2>
-<img src='pictures/schema_championnatM18.png' alt='schéma championnat M18' />-->
+<?php
+$seasonStartYear = date('Y');
+if (date('m') < 9) {
+    $seasonStartYear--;
+}
+$seasonString = $seasonStartYear . '-' . ($seasonStartYear + 1);
+$championshipFormulaImagePath = '/pictures/schema_championnat_' . $seasonString . '.png';
+?>
+<h2>Championnat <?php echo $seasonString; ?></h2>
+<?php
+if (is_file($_SERVER['DOCUMENT_ROOT'] . $championshipFormulaImagePath)) {
+    ?>
+    <img src='<?php echo $championshipFormulaImagePath; ?>' alt='Formule du championnat <?php echo $seasonString; ?>' />
+    <?php
+}
+?>
 
 
