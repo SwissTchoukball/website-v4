27ca00c7ad42a8e3bb270eb9cfd38e9361f0b69a
David Sandoz <david.sandoz@tchoukball.ch>
2016-12-02 19:56:47 +0100

    Applying code style to all "langues" files

--- includes/langue/tab.langue.inc.php
+++ includes/langue/tab.langue.inc.php
@@ -1,8 +1,8 @@
 <?php
 $VAR_TABLEAU_DES_LANGUES = array(
-											array("Fr","Français"),
-											array("De","Deutsch"),
-											array("En","English"),
-											array("It","Italiano"),
-											);
+    array("Fr", "Français"),
+    array("De", "Deutsch"),
+    array("En", "English"),
+    array("It", "Italiano"),
+);
 ?>
