5e7fd726d9cf7bf1ae9336420db004a4910e9fc5
David Sandoz <david.sandoz@tchoukball.ch>
2017-08-23 23:32:17 +0200

    Show a text about the U15 Cellule Espoirs

--- pages/juniorsM15.inc.php
+++ pages/juniorsM15.inc.php
@@ -5,6 +5,14 @@ if ($_SESSION["debug_tracage"]) {
 statInsererPageSurf(__FILE__);
 
 $afficherNumero = false;
+
+$query = "SELECT p.body" . $_SESSION['__langue__'] . " AS body
+        FROM IdTextCorpPage p
+        WHERE p.id = 41";
+$resource = mysql_query($query);
+$pageData = mysql_fetch_assoc($resource);
+$body = Markdown($pageData['body']);
+echo '<p>' . $body . '</p>';
 ?>
 <!--<div class="equipe-es">
     <div class="photo-equipe-es"><img src="<?php /*echo VAR_IMAGE_PHOTOS_EQUIPES_PATH . "m15_2013-02-09.jpg"; */?>"></div>
