f0f7f6da332767b7b2686ea0a021eaaf77942bc3
David Sandoz <david.sandoz@tchoukball.ch>
2017-08-30 20:28:35 +0200

    Use SVG instead of PNG for the season format image and add fallback to last season format if the new season format is not available yet

--- pages/formule.championnat.inc.php
+++ pages/formule.championnat.inc.php
@@ -1,16 +1,27 @@
 <?php
 $seasonStartYear = date('Y');
-if (date('m') < 9) {
+if (date('m') < 7) {
     $seasonStartYear--;
 }
-$seasonString = $seasonStartYear . '-' . ($seasonStartYear + 1);
-$championshipFormulaImagePath = '/pictures/schema_championnat_' . $seasonString . '.png';
+$thisSeason = $seasonStartYear . '-' . ($seasonStartYear + 1);
+$lastSeason = ($seasonStartYear - 1) . '-' . $seasonStartYear;
+
+$shownSeason = $thisSeason;
+
+$championshipFormulaImagePath = '/pictures/schema_championnat_' . $thisSeason . '.svg';
 ?>
-<h2>Championnat <?php echo $seasonString; ?></h2>
 <?php
+if (!is_file($_SERVER['DOCUMENT_ROOT'] . $championshipFormulaImagePath)) {
+    $championshipFormulaImagePath = '/pictures/schema_championnat_' . $lastSeason . '.svg';
+    $shownSeason = $lastSeason;
+}
+
 if (is_file($_SERVER['DOCUMENT_ROOT'] . $championshipFormulaImagePath)) {
     ?>
-    <img src='<?php echo $championshipFormulaImagePath; ?>' alt='Formule du championnat <?php echo $seasonString; ?>'/>
+    <h2>Championnat <?php echo $shownSeason; ?></h2>
+    <img src="<?php echo $championshipFormulaImagePath; ?>"
+         alt="Formule du championnat <?php echo $shownSeason; ?>"
+         class="championship-format-image"/>
     <?php
 }
 ?>
