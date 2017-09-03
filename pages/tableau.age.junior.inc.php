10bf69ca5ff1174a41521fcfa868ab006100c75c
David Sandoz <david.sandoz@tchoukball.ch>
2017-09-03 12:44:19 +0200

    Add "Sport des enfants" categories

--- pages/tableau.age.junior.inc.php
+++ pages/tableau.age.junior.inc.php
@@ -36,8 +36,8 @@ while ($i < 3) {
         <tr>
             <td><?php echo $annee - 20; ?></td>
             <td>20</td>
-            <td rowspan="10">Sport des jeunes</td>
-            <td rowspan="13">Membres juniors</td>
+            <td rowspan="12">Sport des jeunes</td>
+            <td rowspan="20">Membres juniors</td>
         </tr>
         <tr>
             <td><?php echo $annee - 19; ?></td>
@@ -66,31 +66,53 @@ while ($i < 3) {
             <td>14</td>
         </tr>
         <tr>
-            <td><?php echo $annee - 13; ?></td>
-            <td>13</td>
+            <td rowspan="2"><?php echo $annee - 13; ?></td>
+            <td rowspan="2">13</td>
+        </tr>
+        <tr>
+            <td rowspan="3">Noir</td>
         </tr>
         <tr>
             <td><?php echo $annee - 12; ?></td>
             <td>12</td>
-            <td rowspan="2">M12</td>
         </tr>
         <tr>
-            <td><?php echo $annee - 11; ?></td>
-            <td>11</td>
+            <td rowspan="2"><?php echo $annee - 11; ?></td>
+            <td rowspan="2">11</td>
+        </tr>
+        <tr>
+            <td rowspan="3">Rouge</td>
         </tr>
         <tr>
             <td><?php echo $annee - 10; ?></td>
             <td>10</td>
-            <td rowspan="3">M10</td>
-            <td rowspan="3">Sport des enfants</td>
+            <td rowspan="9">Sport des enfants</td>
         </tr>
         <tr>
-            <td><?php echo $annee - 9; ?></td>
-            <td>9</td>
+            <td rowspan="2"><?php echo $annee - 9; ?></td>
+            <td rowspan="2">9</td>
+        </tr>
+        <tr>
+            <td rowspan="3">Bleu</td>
         </tr>
         <tr>
             <td><?php echo $annee - 8; ?></td>
             <td>8</td>
+        </tr>
+        <tr>
+            <td rowspan="2"><?php echo $annee - 7; ?></td>
+            <td rowspan="2">7</td>
+        </tr>
+        <tr>
+            <td rowspan="3">Vert</td>
+        </tr>
+        <tr>
+            <td><?php echo $annee - 6; ?></td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td><?php echo $annee - 5; ?></td>
+            <td>5</td>
         </tr>
     </table>
     <?php
