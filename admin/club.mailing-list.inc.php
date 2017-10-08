2249af2ac4302a8dfc1c2b013474653e32bb7cbd
David Sandoz <david.sandoz@tchoukball.ch>
2017-10-08 16:57:08 +0200

    Add external email addresses to tournament mailing-list

--- admin/club.mailing-list.inc.php
+++ admin/club.mailing-list.inc.php
@@ -1,15 +1,4 @@
 <?php
-$mailingListQuery =
-    "SELECT c.club as shortName, c.emailsOfficialComm, c.emailsTournamentComm
-    FROM clubs c
-    WHERE c.statusId != 3";
-$mailingListResource = mysql_query($mailingListQuery);
-
-$officialCommMailingList = "comite@tchoukball.ch, Comité Swiss Tchoukball\n";
-$officialCommMailingList .= "resp.developpement@tchoukball.ch, Responsable commission développement\n";
-
-$tournamentsCommMailingList = "comite@tchoukball.ch, Comité Swiss Tchoukball\n";
-
 /**
  * Add a list of comma separated email addresses to a string of line separated emails for Infomaniak mailing-list
  * @param $mailingList
@@ -23,10 +12,28 @@ function addToMailingList(&$mailingList, $emails, $clubName) {
     }
 }
 
+$mailingListQuery =
+    "SELECT c.club as shortName, c.emailsOfficialComm, c.emailsTournamentComm
+    FROM clubs c
+    WHERE c.statusId != 3";
+$mailingListResource = mysql_query($mailingListQuery);
+
+$officialCommMailingList = "comite@tchoukball.ch, Comité Swiss Tchoukball\n";
+$officialCommMailingList .= "resp.developpement@tchoukball.ch, Responsable commission développement\n";
+
+$tournamentsCommMailingList = "comite@tchoukball.ch, Comité Swiss Tchoukball\n";
+
 while($club = mysql_fetch_assoc($mailingListResource)) {
     addToMailingList($officialCommMailingList, $club['emailsOfficialComm'], $club['shortName']);
     addToMailingList($tournamentsCommMailingList, $club['emailsTournamentComm'], $club['shortName']);
 }
+
+$externalEmailsTournamentQuery = "SELECT email, name FROM email_tournament_comm_external ORDER BY email";
+$externalEmailsTournamentResource = mysql_query($externalEmailsTournamentQuery);
+while($contact = mysql_fetch_assoc($externalEmailsTournamentResource)) {
+    $tournamentsCommMailingList .= $contact['email']. ',' . $contact['name'] . "\n";
+}
+
 ?>
 <h2>Pour clubs@tchoukball.ch</h2>
 <textarea title="Official communiation mailing-list" class="big-field" readonly><?php echo $officialCommMailingList ?></textarea>
