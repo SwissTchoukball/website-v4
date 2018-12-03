2b45add080921428140a7dc3344092998a864a5c
David Sandoz <david.sandoz@tchoukball.ch>
2018-12-03 06:58:08 +0100

    feat(accounting): add IBAN number and postal address

--- pages/comptabilite.inc.php
+++ pages/comptabilite.inc.php
@@ -6,5 +6,17 @@ statInsererPageSurf(__FILE__);
 
 showDomainHead(9);
 ?>
-<h2>Numéro CCP</h2>
-<p class="ccp-number">20-8957-2</p>
+<h2>Coordonnées bancaires</h2>
+<h3>Numéro CCP</h3>
+<p class="center">
+    <span class="account-number">20-8957-2</span>
+</p>
+<h3>Numéro IBAN</h3>
+<p class="center">
+    <span class="account-number">CH13 0900 0000 2000 8957 2</span>
+</p>
+<h3>Adresse postale</h3>
+<p>
+    Swiss Tchoukball<br>
+    1000 Lausanne
+</p>
