a9bdb90dba43685218e4e6e8b30382d5d49ef820
David Sandoz <david.sandoz@tchoukball.ch>
2019-06-29 19:14:28 +0200

    chore(m18 selection): show back the team picture

--- pages/juniorsM18.inc.php
+++ pages/juniorsM18.inc.php
@@ -7,7 +7,7 @@ statInsererPageSurf(__FILE__);
 $afficherNumero = false;
 ?>
 <div class="equipe-es">
-    <div class="photo-equipe-es"><img src="<?php /*echo VAR_IMAGE_PHOTOS_EQUIPES_PATH . "m18.jpg"; */?>">
+    <div class="photo-equipe-es"><img src="<?php echo VAR_IMAGE_PHOTOS_EQUIPES_PATH . "m18.jpg"; ?>">
     </div>
     <div class="legende-photo-equipe-es"></div>
 </div>
