42182acd5734cfe9c22f9756b6bf62459b0aba54
David Sandoz <david.sandoz@tchoukball.ch>
2019-08-21 16:14:52 +0200

    feat(wtc): add country to location of WTC editions

--- pages/international.wtc.inc.php
+++ pages/international.wtc.inc.php
@@ -15,47 +15,47 @@ statInsererPageSurf(__FILE__);
     <tr>
         <td>5èmes</td>
         <td>Salle</td>
-        <td>Nilai, 2019</td>
+        <td>Nilai (Malaisie) 2019</td>
     </tr>
     <tr>
         <td>4èmes</td>
         <td>Salle</td>
-        <td>Ferrara, 2011</td>
+        <td>Ferrara (Italie) 2011</td>
     </tr>
     <tr>
         <td>3èmes et 5èmes</td>
         <td>Sable</td>
-        <td>Genève, 2005</td>
+        <td>Genève (Suisse) 2005</td>
     </tr>
     <tr>
         <td>1ers</td>
         <td>Salle</td>
-        <td>Kaohsiung, 2004</td>
+        <td>Kaohsiung (Taïwan) 2004</td>
     </tr>
     <tr>
         <td>2èmes</td>
         <td>Salle</td>
-        <td>Loughborough, 2002</td>
+        <td>Loughborough (Royaume Uni) 2002</td>
     </tr>
     <tr>
         <td>3èmes</td>
         <td>Salle</td>
-        <td>Genève, 2000</td>
+        <td>Genève (Suisse) 2000</td>
     </tr>
     <tr>
         <td>5èmes</td>
         <td>Salle</td>
-        <td>Portsmouth, 1990</td>
+        <td>Portsmouth (Royaume Uni) 1990</td>
     </tr>
     <tr>
         <td>5èmes</td>
         <td>Salle</td>
-        <td>Neuchâtel, 1987</td>
+        <td>Neuchâtel (Suisse) 1987</td>
     </tr>
     <tr>
         <td>7èmes</td>
         <td>Salle</td>
-        <td>Taipei, 1984</td>
+        <td>Taipei (Taïwan) 1984</td>
     </tr>
 </table>
 <h3 class="alt">Femmes</h3>
@@ -68,42 +68,42 @@ statInsererPageSurf(__FILE__);
     <tr>
         <td>4èmes</td>
         <td>Salle</td>
-        <td>Nilai, 2019</td>
+        <td>Nilai (Malaisie) 2019</td>
     </tr>
     <tr>
         <td>2èmes</td>
         <td>Salle</td>
-        <td>Ferrara, 2011</td>
+        <td>Ferrara (Italie) 2011</td>
     </tr>
     <tr>
         <td>3èmes et 4èmes</td>
         <td>Sable</td>
-        <td>Genève, 2005</td>
+        <td>Genève (Suisse) 2005</td>
     </tr>
     <tr>
         <td>2èmes</td>
         <td>Salle</td>
-        <td>Kaohsiung, 2004</td>
+        <td>Kaohsiung (Taïwan) 2004</td>
     </tr>
     <tr>
         <td>2èmes</td>
         <td>Salle</td>
-        <td>Loughborough, 2002</td>
+        <td>Loughborough (Royaume Uni) 2002</td>
     </tr>
     <tr>
         <td>3èmes</td>
         <td>Salle</td>
-        <td>Genève, 2000</td>
+        <td>Genève (Suisse) 2000</td>
     </tr>
     <tr>
         <td>4èmes</td>
         <td>Salle</td>
-        <td>Portsmouth, 1990</td>
+        <td>Portsmouth (Royaume Uni) 1990</td>
     </tr>
     <tr>
         <td>4èmes</td>
         <td>Salle</td>
-        <td>Neuchâtel, 1987</td>
+        <td>Neuchâtel (Suisse) 1987</td>
     </tr>
 </table>
 <h3 class="alt">M18 Hommes ou M18 Mixte</h3>
@@ -116,22 +116,22 @@ statInsererPageSurf(__FILE__);
     <tr>
         <td>4èmes</td>
         <td>Salle</td>
-        <td>Singapour, 2019</td>
+        <td>Singapour (Singapour) 2019</td>
     </tr>
     <tr>
         <td>3èmes</td>
         <td>Salle</td>
-        <td>Kaohsiung, 2013</td>
+        <td>Kaohsiung (Taïwan) 2013</td>
     </tr>
     <tr>
         <td>2èmes</td>
         <td>Salle</td>
-        <td>Traiskirchen, 2011</td>
+        <td>Traiskirchen (Autriche), 2011</td>
     </tr>
     <tr>
         <td>3èmes et 4èmes</td>
         <td>Sable</td>
-        <td>Genève, 2005</td>
+        <td>Genève (Suisse) 2005</td>
     </tr>
 </table>
 <h3 class="alt">M18 Femmes</h3>
@@ -144,12 +144,12 @@ statInsererPageSurf(__FILE__);
     <tr>
         <td>3èmes</td>
         <td>Salle</td>
-        <td>Kaohsiung, 2013</td>
+        <td>Kaohsiung (Taïwan) 2013</td>
     </tr>
     <tr>
         <td>1ères</td>
         <td>Sable</td>
-        <td>Genève, 2005</td>
+        <td>Genève (Suisse) 2005</td>
     </tr>
 </table>
 <h3 class="alt">M15 Hommes</h3>
@@ -162,6 +162,6 @@ statInsererPageSurf(__FILE__);
     <tr>
         <td>4èmes</td>
         <td>Salle</td>
-        <td>Kaohsiung, 2013</td>
+        <td>Kaohsiung (Taïwan) 2013</td>
     </tr>
 </table>
