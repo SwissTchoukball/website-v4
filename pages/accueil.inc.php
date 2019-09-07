455241c60e5194bb0ec548eb94f30c90015bd8d6
David Sandoz <david.sandoz@tchoukball.ch>
2019-09-07 21:17:02 +0200

    feat(home): hide announcement

--- pages/accueil.inc.php
+++ pages/accueil.inc.php
@@ -1,5 +1,5 @@
 <?php
-    $showAnnouncement = true;
+    $showAnnouncement = false;
 ?>
 <div class="homepage <?php echo $showAnnouncement ? 'homepage--with-announcement' : ''; ?>">
     <div class="homepage-block homepage-block--announcement <?php echo $showAnnouncement ? '' : 'homepage-block--hidden'; ?>">
