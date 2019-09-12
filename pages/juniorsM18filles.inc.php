df0362403e90fe6edb8e1c28435895863d782a0c
David Sandoz <david.sandoz@tchoukball.ch>
2019-09-12 22:04:09 +0200

    feat(national squad): add girls M18 team page

---
+++ pages/juniorsM18filles.inc.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SESSION["debug_tracage"]) {
+    echo __FILE__ . "<BR>";
+}
+statInsererPageSurf(__FILE__);
+
+$afficherNumero = false;
+?>
+<!--<div class="equipe-es">
+    <div class="photo-equipe-es"><img src="<?php /*echo VAR_IMAGE_PHOTOS_EQUIPES_PATH . "m18.jpg"; */?>">
+    </div>
+    <div class="legende-photo-equipe-es"></div>
+</div>-->
+
+<div class="liste-joueur-es">
+    <?php
+    include "affichage.team.inc.php";
+
+    $query = getTeamQuery(5);
+    $recordset = mysql_query($query) or die ("<H1>mauvaise requete</H1>");
+
+    while ($record = mysql_fetch_array($recordset)) {
+        afficherPersonneTeam($record, "_port", $afficherNumero);
+    }
+    ?>
+</div>
+
+<?php
+showTeamCoaches(5);
+showCommissionHead(9);
