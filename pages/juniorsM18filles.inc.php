c864543ebea1ba123a5a30a22e9bdf6365cf9ba6
David Sandoz <david.sandoz@tchoukball.ch>
2019-09-12 22:07:14 +0200

    feat(national squad): hide players of M18 teams

--- pages/juniorsM18filles.inc.php
+++ pages/juniorsM18filles.inc.php
@@ -14,14 +14,14 @@ $afficherNumero = false;
 
 <div class="liste-joueur-es">
     <?php
-    include "affichage.team.inc.php";
-
-    $query = getTeamQuery(5);
-    $recordset = mysql_query($query) or die ("<H1>mauvaise requete</H1>");
-
-    while ($record = mysql_fetch_array($recordset)) {
-        afficherPersonneTeam($record, "_port", $afficherNumero);
-    }
+//    include "affichage.team.inc.php";
+//
+//    $query = getTeamQuery(5);
+//    $recordset = mysql_query($query) or die ("<H1>mauvaise requete</H1>");
+//
+//    while ($record = mysql_fetch_array($recordset)) {
+//        afficherPersonneTeam($record, "_port", $afficherNumero);
+//    }
     ?>
 </div>
 
