814a17e82a59aea32fe161e9bd4682068e25c292
David Sandoz <david.sandoz@tchoukball.ch>
2019-10-18 21:08:41 +0200

    feat(contact): update some contacts and refresh layout

--- pages/contact.inc.php
+++ pages/contact.inc.php
@@ -22,20 +22,24 @@ statInsererPageSurf(__FILE__);
     <div class="contenuContact"><a href="/comite-executif">Page des membres du comité</a></div>
     <div class="typeContact">Commissions</div>
     <div class="contenuContact"><a href="/commissions">Page des commissions</a></div>
-    <div class="typeContact">Responsable Communication</div>
+    <div class="typeContact">Responsable communication</div>
     <div class="contenuContact"><?php echo email("communication@tchoukball.ch"); ?></div>
-    <div class="typeContact">Responsable Sponsoring</div>
+    <div class="typeContact">Responsable sponsoring</div>
     <div class="contenuContact"><?php echo email("sponsoring@tchoukball.ch"); ?></div>
-    <div class="typeContact">Responsable Compétitions</div>
-    <div class="contenuContact"><?php echo email("competitions@tchoukball.ch"); ?></div>
-    <div class="typeContact">Responsable Arbitrage</div>
+    <div class="typeContact">Responsable technique</div>
+    <div class="contenuContact"><?php echo email("technique@tchoukball.ch"); ?></div>
+    <div class="typeContact">Responsable Championnat suisse</div>
+    <div class="contenuContact"><?php echo email("championnat@tchoukball.ch"); ?></div>
+    <div class="typeContact">Responsable Coupe suisse</div>
+    <div class="contenuContact"><?php echo email("coupesuisse@tchoukball.ch"); ?></div>
+    <div class="typeContact">Responsable arbitrage</div>
     <div class="contenuContact"><?php echo email("arbitrage@tchoukball.ch"); ?></div>
     <div class="typeContact">Juniors</div>
     <div class="contenuContact"><?php echo email("juniors@tchoukball.ch"); ?></div>
-    <div class="typeContact" title="Photo et vidéo">Multimédia</div>
+    <div class="typeContact">Multimédia</div>
     <div class="contenuContact"><?php echo email("multimedia@tchoukball.ch"); ?></div>
-    <div class="typeContact">Rédaction du tchouk<sup>up</sup></div>
+    <div class="typeContact">Rédaction du Tchouk<sup>up</sup></div>
     <div class="contenuContact"><?php echo email("redaction@tchoukball.ch"); ?></div>
-    <div class="typeContact">Webmaster</div>
-    <div class="contenuContact"><?php echo email("webmaster@tchoukball.ch"); ?></div>
+    <div class="typeContact">Administrateur</div>
+    <div class="contenuContact"><?php echo email("admin@tchoukball.ch"); ?></div>
 </div>
