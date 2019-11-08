627b1012a19aa4eb1e8b00eface03717f576ba4b
David Sandoz <david.sandoz@tchoukball.ch>
2019-11-08 16:46:40 +0100

    feat(championship): separate formula per league

--- pages/formule.championnat.inc.php
+++ pages/formule.championnat.inc.php
@@ -8,20 +8,38 @@ $lastSeason = ($seasonStartYear - 1) . '-' . $seasonStartYear;
 
 $shownSeason = $thisSeason;
 
-$championshipFormulaImagePath = '/pictures/schema_championnat_' . $thisSeason . '.svg';
-?>
-<?php
-if (!is_file($_SERVER['DOCUMENT_ROOT'] . $championshipFormulaImagePath)) {
-    $championshipFormulaImagePath = '/pictures/schema_championnat_' . $lastSeason . '.svg';
+$leagueAFormulaImagePath = '/pictures/schema_championnat_%s.svg';
+if (!is_file($_SERVER['DOCUMENT_ROOT'] . sprintf($leagueAFormulaImagePath, $thisSeason))) {
+    $leagueAFormulaImagePath = sprintf($leagueAFormulaImagePath, $lastSeason);
     $shownSeason = $lastSeason;
 }
 
-if (is_file($_SERVER['DOCUMENT_ROOT'] . $championshipFormulaImagePath)) {
+$leagueBFormulaImagePath = '/pictures/schema_championnat_%s.svg';
+if (!is_file($_SERVER['DOCUMENT_ROOT'] . sprintf($leagueBFormulaImagePath, $thisSeason))) {
+    $leagueBFormulaImagePath = sprintf($leagueBFormulaImagePath, $lastSeason);
+    $shownSeason = $lastSeason;
+}
+
+$promotionRelegationFormulaImagePath = '/pictures/schema_championnat_%s.svg';
+if (!is_file($_SERVER['DOCUMENT_ROOT'] . sprintf($promotionRelegationFormulaImagePath, $thisSeason))) {
+    $promotionRelegationFormulaImagePath = sprintf($promotionRelegationFormulaImagePath, $lastSeason);
+    $shownSeason = $lastSeason;
+}
+
+if (is_file($_SERVER['DOCUMENT_ROOT'] . $leagueAFormulaImagePath)) {
     ?>
     <h2>Championnat <?php echo $shownSeason; ?></h2>
-    <img src="<?php echo $championshipFormulaImagePath; ?>"
-         alt="Formule du championnat <?php echo $shownSeason; ?>"
-         class="championship-format-image"/>
+    <h3>Ligue A</h3>
+    <img src="<?php printf($leagueAFormulaImagePath, $shownSeason); ?>"
+         alt="Formule de la ligue A du championnat <?php echo $shownSeason; ?>"/>
+
+    <h3>Ligue B</h3>
+    <img src="<?php printf($leagueBFormulaImagePath, $shownSeason); ?>"
+         alt="Formule de la ligue B du championnat <?php echo $shownSeason; ?>"/>
+
+    <h3>Promotion/relégation</h3>
+    <img src="<?php printf($promotionRelegationFormulaImagePath, $shownSeason); ?>"
+         alt="Formule de la promotion/relégation du championnat <?php echo $shownSeason; ?>"/>
     <?php
 }
 ?>
