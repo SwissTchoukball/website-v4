c16aff730b6a0ebcd0a67d0f25e245cbd6848807
David Sandoz <david.sandoz@tchoukball.ch>
2021-07-30 14:39:40 +0200

    feat: add U15 regional selections page

---
+++ pages/selections.regionales.m15.inc.php
@@ -0,0 +1,11 @@
+<?php
+if ($_SESSION["debug_tracage"]) {
+    echo __FILE__ . "<BR>";
+}
+statInsererPageSurf(__FILE__);
+
+echo '<div>' . getSimplePageContent(17) . '</div>';
+
+showFunctionPerson(12);
+showFunctionPerson(13);
+showCommissionHead(9);
