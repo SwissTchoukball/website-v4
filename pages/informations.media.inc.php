e04cec89bc6d61729dc2a19c9f461bd6a31a0340
David Sandoz <david.sandoz@tchoukball.ch>
2022-03-14 20:18:33 +0100

    fix(press releases): hide list
    We rather show a link to a folder containing all the press releases

--- pages/informations.media.inc.php
+++ pages/informations.media.inc.php
@@ -11,32 +11,36 @@ while ($donnees = mysql_fetch_array($retour)) {
         echo "<h4>";
         afficherAvecEncryptageEmail($donnees["paragraphe" . $_SESSION["__langue__"]]);
         echo "</h4><br />";
+    } else {
+        echo "<p>";
+        afficherAvecEncryptageEmail($donnees["paragraphe" . $_SESSION["__langue__"]]);
+        echo "</p><br />";
     }
 }
 
 
-$requeteSQL = "SELECT * FROM `Download` WHERE `idType`='1' ORDER BY `date` DESC";
-$recordset = mysql_query($requeteSQL) or die ("<p>internal error</p>");
+// $requeteSQL = "SELECT * FROM `Download` WHERE `idType`='1' ORDER BY `date` DESC";
+// $recordset = mysql_query($requeteSQL) or die ("<p>internal error</p>");
 
-$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+// $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 
-$nbFois = 0;
-echo "<ul>";
-while ($record = mysql_fetch_array($recordset)) {
-    if ($nbFois > 0) {
-        $nbFois++;
-    }
-    $pressReleaseUrl = PATH_DOCUMENTS . $VAR_TABLEAU_DES_LANGUES[0][0] . "_" . $record["fichier"];
-    $pressReleaseTitle = $record["titre" . $_SESSION["__langue__"]];
-    if (is_file($root . PATH_DOCUMENTS . $_SESSION["__langue__"] . "_" . $record["fichier"])) {
-        $pressReleaseUrl = PATH_DOCUMENTS . $_SESSION["__langue__"] . "_" . $record["fichier"];
-    }
+// $nbFois = 0;
+// echo "<ul>";
+// while ($record = mysql_fetch_array($recordset)) {
+//     if ($nbFois > 0) {
+//         $nbFois++;
+//     }
+//     $pressReleaseUrl = PATH_DOCUMENTS . $VAR_TABLEAU_DES_LANGUES[0][0] . "_" . $record["fichier"];
+//     $pressReleaseTitle = $record["titre" . $_SESSION["__langue__"]];
+//     if (is_file($root . PATH_DOCUMENTS . $_SESSION["__langue__"] . "_" . $record["fichier"])) {
+//         $pressReleaseUrl = PATH_DOCUMENTS . $_SESSION["__langue__"] . "_" . $record["fichier"];
+//     }
 
-    // We don't add the path prefix if it is already a full path
-    if (preg_match('#^https?:\/\/#i', $record["fichier"]) === 1) {
-        $pressReleaseUrl = $record["fichier"];
-    }
+//     // We don't add the path prefix if it is already a full path
+//     if (preg_match('#^https?:\/\/#i', $record["fichier"]) === 1) {
+//         $pressReleaseUrl = $record["fichier"];
+//     }
 
-    echo "<li>" . date_sql2date($record["date"]) . " : <a href='" . $pressReleaseUrl . "'>" . $pressReleaseTitle . "</a></li>";
-}
-echo "</ul>";
+//     echo "<li>" . date_sql2date($record["date"]) . " : <a href='" . $pressReleaseUrl . "'>" . $pressReleaseTitle . "</a></li>";
+// }
+// echo "</ul>";
